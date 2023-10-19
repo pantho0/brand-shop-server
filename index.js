e5c13de388334d16b641734c396085cdd7e65ac1
pantho0 <panthomashreky6@gmail.com>
2023-10-19 20:23:58 +0600

    post method added for add product

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 // middleware
 app.use(cors());
@@ -29,12 +29,29 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const productCollection = client.db("brandDB").collection('productDB');
+
+
+    app.post('/addproduct', async(req, res)=>{
+        const product = req.body;
+        console.log(product);
+        const result = await productCollection.insertOne(product)
+        res.send(result)
+    })  
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
